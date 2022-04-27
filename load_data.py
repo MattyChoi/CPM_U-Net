fixed the backpropagation error

load_data.py
@@ -1,3 +1,4 @@
+from cv2 import CAP_PROP_OPENNI_CIRCLE_BUFFER
 from torch.utils.data import Dataset
 import torch.utils.data as torchdata
 import torch
@@ -46,7 +47,7 @@ class OMC(Dataset):
         cen_crop = np.array(cen_crop)
         
         train_img = np.transpose(img_crop, (2,0,1))/255.0
-
+        
         train_heatmaps = utils.gen_hmaps(np.zeros(input_shape), pts_crop)
         train_heatmaps = np.transpose(train_heatmaps, (2,0,1))
 

misc_nets/cpm.py
@@ -14,14 +14,14 @@ class CPM(nn.Module):
         self.stageT = CPM_StageT(num_joints)
 
     def forward(self, image, center_map):
-        pooled_cmap = self.avg_pool(center_map)
+        pooled_cmap = self.pool_center(center_map)
         stage1_maps = self.stage1(image)
         features = self.features(image)
 
         self.heatmaps.append(stage1_maps)
-
+        
         for _ in range(self.num_stages - 2):
-            cur_map = self.stageT(self.heatmaps[-1], features, pooled_cmap)
+            cur_map = self.stageT(features, self.heatmaps[-1], pooled_cmap)
             self.heatmaps.append(cur_map)
 
         return self.heatmaps
@@ -29,7 +29,7 @@ class CPM(nn.Module):
 
 class CPM_ImageFeatures(nn.Module):
     def __init__(self):
-        super(CPM_ImageFeatures, self).__init__()
+        super().__init__()
         self.conv1 = nn.Conv2d(3, 128, kernel_size=9, padding=4)
         self.pool1 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.conv2 = nn.Conv2d(128, 128, kernel_size=9, padding=4)
@@ -48,7 +48,7 @@ class CPM_ImageFeatures(nn.Module):
 
 class CPM_Stage1(nn.Module):
     def __init__(self, num_joints):
-        super(CPM_Stage1, self).__init__()
+        super().__init__()
         self.num_joints = num_joints
 
         self.features = CPM_ImageFeatures()
@@ -66,20 +66,20 @@ class CPM_Stage1(nn.Module):
 
 class CPM_StageT(nn.Module):
     def __init__(self, num_joints):
-        super(CPM_StageT, self).__init__()
+        super().__init__()
         self.num_joints = num_joints
 
-        self.conv_image = nn.Conv2d(128, 32, kernel_size=5, padding=2)
+        # self.conv_image = nn.Conv2d(128, 32, kernel_size=5, padding=2)
 
-        self.conv1 = nn.Conv2d(32 + self.k + 2, 128, kernel_size=11, padding=5)
+        self.conv1 = nn.Conv2d(32 + self.num_joints + 2, 128, kernel_size=11, padding=5)
         self.conv2 = nn.Conv2d(128, 128, kernel_size=11, padding=5)
         self.conv3 = nn.Conv2d(128, 128, kernel_size=11, padding=5)
         self.conv4 = nn.Conv2d(128, 128, kernel_size=1, padding=0)
         self.conv5 = nn.Conv2d(128, self.num_joints + 1, kernel_size=1, padding=0)
 
-    def forward(self, prev_map, features, center_map):
-        x = F.relu(self.conv_image(prev_map))
-        x = torch.cat([features, x, center_map], dim=1)
+    def forward(self, features, prev_map, center_map):
+        # x = F.relu(self.conv_image(features))
+        x = torch.cat([prev_map, features, center_map], dim=1)
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
         x = F.relu(self.conv3(x))

net.py
@@ -8,8 +8,7 @@ class CPM_UNet(nn.Module):
     def __init__(self, num_stages, num_joints):
         super(CPM_UNet, self).__init__()
         self.num_stages = num_stages
-        self.heatmaps = []
-
+        
         # replace self.features with a unet architecture
         # self.features = CPM_ImageFeatures()
         self.features = UNet(3)
@@ -18,16 +17,17 @@ class CPM_UNet(nn.Module):
         self.stageT = CPM_StageT(num_joints)
 
     def forward(self, image, center_map):
+        heatmaps = []
         stage1_maps = self.stage1(image)
         features = self.features(image)
 
-        self.heatmaps.append(stage1_maps)
+        heatmaps.append(stage1_maps)
 
         for _ in range(self.num_stages - 1):
-            cur_map = self.stageT(features, self.heatmaps[-1], center_map)
-            self.heatmaps.append(cur_map)
+            cur_map = self.stageT(features, heatmaps[-1], center_map)
+            heatmaps.append(cur_map)
 
-        return self.heatmaps
+        return heatmaps
 
 
 class CPM_ImageFeatures(nn.Module):
@@ -39,11 +39,13 @@ class CPM_ImageFeatures(nn.Module):
         self.pool2 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.conv3 = nn.Conv2d(128, 128, kernel_size=9, padding=4)
         self.pool3 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.conv4 = nn.Conv2d(128, 32, kernel_size=5, padding=2)
 
     def forward(self, x):
         x = self.pool1(F.relu(self.conv1(x)))
         x = self.pool2(F.relu(self.conv2(x)))
         x = self.pool3(F.relu(self.conv3(x)))
+        x = F.relu(self.conv4(x))
         return x
 
 

unet.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 
 class UNet(nn.Module):
     def __init__(self, n_channels):
-        super(UNet, self).__init__()
+        super().__init__()
         self.n_channels = n_channels
 
         self.inc = Block(n_channels, 64)